946fe5d32220a9cb2a1b577e8bd8bf0e0bbac480
JawadMM <almuttawa.j@hotmail.com>
2023-07-11 16:11:30 +0300

    caompground model is created

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const expresss = require("express");
-const app = expresss();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = expresss();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    name: "My Backyard",
+    description: "Cheep Camping!",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
