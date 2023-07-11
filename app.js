a493b258bab1a8b49baf2c74e9241533b5acb280
JawadMM <almuttawa.j@hotmail.com>
2023-07-11 11:23:20 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const expresss = require("express");
+const app = expresss();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("LISTENING ON PORT 3000");
+});
