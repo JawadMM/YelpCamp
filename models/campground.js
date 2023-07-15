b4c9fe1875dc9704dd433be4fab29ee9747e2164
JawadMM <almuttawa.j@hotmail.com>
2023-07-15 14:20:50 +0300

    testing seed data

--- models/campground.js
+++ models/campground.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const campgroundSchema = new Schema({
-  name: String,
+  title: String,
   price: String,
   description: String,
   location: String,
